bfd54a19e02732fb8a32dbfee83d71c50507e617
sunilkpai <sunilpai@stanford.edu>
2021-10-29 13:42:37 -0700

    adding some requirements for app deployment

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ project_name = "phox"
 setup(
     name=project_name,
     version="0.1",
-    packages=[project_name]
+    packages=[project_name],
+    install_requires=[
+        'numpy>=1.19',
+        'scipy>=1.7.1',
+        'dphox',
+        'simphox'
+    ],
 )
