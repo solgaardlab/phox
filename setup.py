c225b2e9f94779495100ea69aab187e07b042df1
sunilkpai <sunilpai@stanford.edu>
2021-10-29 14:15:01 -0700

    adding holoviews package for deployment

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 project_name = "phox"
 
 setup(
     name=project_name,
     version="0.1",
-    packages=[project_name],
+    packages=find_packages(),
     install_requires=[
         'numpy>=1.19',
         'scipy>=1.7.1',
         'dphox>=0.0.1a4',
         'simphox>=0.0.1a3',
-        'xarray'
+        'holoviews'
     ],
 )
