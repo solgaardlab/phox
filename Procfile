fed77711a2eb7d9eecc4e450a85d6803d603109e
sunilkpai <sunilpai@stanford.edu>
2021-10-29 13:33:54 -0700

    adding simulation for heroku hosting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python serve_sim.py $PORT
---
+++ serve_sim.py
@@ -0,0 +1,17 @@
+from simphox.utils import random_unitary
+from simphox.circuit import vector_unit
+from phox.model.mesh import Mesh
+import holoviews as hv
+import panel as pn
+
+if __name__ == '__main__':
+    hv.extension('bokeh')
+    u = random_unitary(8)
+    mesh = Mesh(vector_unit(u))
+
+    app = mesh.hvsim(height=200, wide=True)
+
+    pn.serve(app, start=True, show=False, port=5006,
+        websocket_origin="localhost:4444",
+        ssl_certfile='/home/exx/ssl/jupyter_cert.pem',
+        ssl_keyfile='/home/exx/ssl/jupyter_cert.key')
