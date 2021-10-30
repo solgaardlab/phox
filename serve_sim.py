595a0227c851b312139b3640608418f0d7ad2194
sunilkpai <sunilpai@stanford.edu>
2021-10-29 17:43:33 -0700

    trying to allow multiuser apps

--- serve_sim.py
+++ serve_sim.py
@@ -8,27 +8,33 @@ import sys
 if __name__ == '__main__':
     hv.extension('bokeh')
 
-    def get_page_user():
+    def mzi():
         u2 = random_unitary(2)
         mesh = Mesh(vector_unit(u2)[0])
         mesh.v = u2.T[-1]
-        mzi = mesh.hvsim(height=200, wide=True, title='MZI')
+        return mesh.hvsim(height=200, wide=True, title='MZI')
+
+    def bt4():
         u4 = random_unitary(4)
         mesh = Mesh(vector_unit(u4)[0])
         mesh.v = u4.T[-1]
-        bt4 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=4')
+        return mesh.hvsim(height=200, wide=True, title='Binary tree with N=4')
+
+    def bt8():
         u8 = random_unitary(8)
         mesh = Mesh(vector_unit(u8)[0])
         mesh.v = u8.T[-1]
-        bt8 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=8')
+        return mesh.hvsim(height=200, wide=True, title='Binary tree with N=8')
+
+    def lc8():
+        u8 = random_unitary(8)
         mesh = Mesh(vector_unit(u8, balanced=False)[0])
         mesh.v = u8.T[-1]
-        lc8 = mesh.hvsim(height=200, wide=True, title='Linear change with N=8')
-        return {
-            'MZI': mzi,
-            'BalancedTree4': bt4,
-            'BalancedTree8': bt8,
-            'LinearChain8': lc8
-        }
+        return mesh.hvsim(height=200, wide=True, title='Linear change with N=8')
 
-    pn.serve(get_page_user, start=True, show=True, port=int(sys.argv[-1]), websocket_origin='phoxmesh.herokuapp.com')
+    pn.serve({
+        'MZI': mzi,
+        'BalancedTree4': bt4,
+        'BalancedTree8': bt8,
+        'LinearChain8': lc8
+    }, start=True, show=True, port=int(sys.argv[-1]), websocket_origin='phoxmesh.herokuapp.com')
