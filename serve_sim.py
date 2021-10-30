692700800f733e21a328ebe0d4016c9b584cb269
sunilkpai <sunilpai@stanford.edu>
2021-10-29 17:37:35 -0700

    attempting to fix multiuser issue

--- serve_sim.py
+++ serve_sim.py
@@ -7,25 +7,28 @@ import sys
 
 if __name__ == '__main__':
     hv.extension('bokeh')
-    u2 = random_unitary(2)
-    mesh = Mesh(vector_unit(u2)[0])
-    mesh.v = u2.T[-1]
-    mzi = mesh.hvsim(height=200, wide=True, title='MZI')
-    u4 = random_unitary(4)
-    mesh = Mesh(vector_unit(u4)[0])
-    mesh.v = u4.T[-1]
-    bt4 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=4')
-    u8 = random_unitary(8)
-    mesh = Mesh(vector_unit(u8)[0])
-    mesh.v = u8.T[-1]
-    bt8 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=8')
-    mesh = Mesh(vector_unit(u8, balanced=False)[0])
-    mesh.v = u8.T[-1]
-    lc8 = mesh.hvsim(height=200, wide=True, title='Linear change with N=8')
 
-    pn.serve({
-        'MZI': mzi,
-        'BalancedTree4': bt4,
-        'BalancedTree8': bt8,
-        'LinearChain8': lc8
-    }, start=True, show=True, port=int(sys.argv[-1]), websocket_origin='phoxmesh.herokuapp.com')
+    def get_page_user():
+        u2 = random_unitary(2)
+        mesh = Mesh(vector_unit(u2)[0])
+        mesh.v = u2.T[-1]
+        mzi = mesh.hvsim(height=200, wide=True, title='MZI')
+        u4 = random_unitary(4)
+        mesh = Mesh(vector_unit(u4)[0])
+        mesh.v = u4.T[-1]
+        bt4 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=4')
+        u8 = random_unitary(8)
+        mesh = Mesh(vector_unit(u8)[0])
+        mesh.v = u8.T[-1]
+        bt8 = mesh.hvsim(height=200, wide=True, title='Binary tree with N=8')
+        mesh = Mesh(vector_unit(u8, balanced=False)[0])
+        mesh.v = u8.T[-1]
+        lc8 = mesh.hvsim(height=200, wide=True, title='Linear change with N=8')
+        return {
+            'MZI': mzi,
+            'BalancedTree4': bt4,
+            'BalancedTree8': bt8,
+            'LinearChain8': lc8
+        }
+
+    pn.serve(get_page_user, start=True, show=True, port=int(sys.argv[-1]), websocket_origin='phoxmesh.herokuapp.com')
