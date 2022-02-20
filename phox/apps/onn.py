fixing errors with backprop, seems to work now

phox/apps/onn.py
@@ -168,12 +168,12 @@ class BackpropAccuracyTest:
         for layer in (1, 2, 3):
             self.chip.set_unitary_phases(self.onn_layers[f'layer{layer}'].thetas, self.onn_layers[f'layer{layer}'].phis)
             self.meas[f'forward_{layer}'] = self.chip.matrix_prop(self.meas[f'input_{layer}'])
+            self.meas[f'input_{layer + 1}'] = np.sqrt(np.abs(self.meas[f'forward_{layer}'][-1][:, :4]))
             if not phase_cheat:
-                self.chip.set_input(np.hstack((self.meas[f'input_{layer}'], 1)))
-                self.meas[f'input_{layer + 1}'] = self.chip.coherent_batch(self.meas[f'input_{layer}'])
+                self.meas[f'forward_out_{layer}'] = self.chip.coherent_batch(self.meas[f'input_{layer}'])
             else:
-                self.meas[f'input_{layer + 1}'] = np.sqrt(np.abs(self.meas[f'forward_{layer}'][-1][:, :4]))
                 self.meas[f'forward_out_{layer}'] = self.pred[f'forward_{layer}'][-1]
+            self.chip.set_output_transparent()
 
     def _backward_measure(self, phase_cheat: bool = False):
         cost_fn = [optical_softmax_cross_entropy(self.y[idx]) for idx in self.idx_list]
@@ -192,6 +192,7 @@ class BackpropAccuracyTest:
                 self.meas[f'backward_out_{layer}'] = self.chip.coherent_batch(self.meas[f'error_{layer}'])
             backward_phasors = np.exp(1j * np.angle(self.meas[f'backward_out_{layer}']))
             self.meas[f'adjoint_{layer}'] = np.sqrt(np.abs(self.meas[f'backward_{layer}'][0][:, :4])) * backward_phasors
+            self.chip.set_output_transparent()
         self.chip.toggle_propagation_direction()
 
     def run(self, phase_cheat: bool = False):

phox/experiment/amf420mesh.py
@@ -573,7 +573,7 @@ class AMF420Mesh(ActivePhotonicsImager):
         _, _, gammas = mesh.params
         mesh.params = thetas[::-1], phis[::-1], np.zeros_like(gammas)
 
-        return mesh.matrix()[-1][:5][::-1].conj()
+        return mesh.matrix()[-1][::-1].conj()
 
     def coherent_batch(self, vs: np.ndarray, wait_time: float = 0.02, coherent_4_alpha: float = 1):
         outputs = []
@@ -583,7 +583,7 @@ class AMF420Mesh(ActivePhotonicsImager):
             y = self.output_from_analyzer
             if coherent_4_alpha != 0:
                 y = y[:4] / -np.exp(1j * np.angle(y[-1]))
-                y = y / np.linalg.norm(y)
+            y = y / np.linalg.norm(y)
             outputs.append(y * np.linalg.norm(v))
         return np.array(outputs)
 